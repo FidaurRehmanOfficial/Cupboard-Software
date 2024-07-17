3e4e38849960b16ee0574ab16bea2b809e9e8184
FidaurRehmanOfficial <fidaurrehman23@gmail.com>
2024-07-17 15:56:33 +0000

    2

--- cupboard/business/views.py
+++ cupboard/business/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Invoice
+from .forms import InvoiceForm
 
 # Create your views here.
+
+def create_invoice(request):
+    if request.method == 'POST':
+        form = InvoiceForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('invoice_list')
+    else:
+        form = InvoiceForm()
+    return render(request, 'business/create_invoice.html', {'form': form})
+
+def invoice_list(request):
+    invoices = Invoice.objects.all()
+    return render(request, 'business/invoice_list.html', {'invoices': invoices})
