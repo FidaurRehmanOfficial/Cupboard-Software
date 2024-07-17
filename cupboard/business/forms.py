3f858490e1a13dd18e7bfeca54f77d31ef8c320c
FidaurRehmanOfficial <fidaurrehman23@gmail.com>
2024-07-17 16:03:02 +0000

    3

--- cupboard/business/forms.py
+++ cupboard/business/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Invoice, InventoryItem
+from .models import Invoice, InventoryItem, Expense, Payment
 
 class InvoiceForm(forms.ModelForm):
     class Meta:
@@ -9,3 +9,12 @@ class InventoryItemForm(forms.ModelForm):
     class Meta:
         model = InventoryItem
         fields = ['name', 'quantity', 'purchase_price', 'selling_price']
+
+class ExpenseForm(forms.ModelForm):
+    class Meta:
+        model = Expense
+        fields = ['description', 'amount']
+class PaymentForm(forms.ModelForm):
+    class Meta:
+        model = Payment
+        fields = ['invoice', 'amount', 'method']        
--- cupboard/business/views.py
+++ cupboard/business/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from .models import Invoice, InventoryItem
-from .forms import InvoiceForm, InventoryItemForm
+from .models import Invoice, InventoryItem, Expense, Payment
+from .forms import InvoiceForm, InventoryItemForm, ExpenseForm, PaymentForm
+from django.db.models import Sum
 
 # Create your views here.
 
@@ -30,3 +31,39 @@ def create_inventory_item(request):
 def inventory_list(request):
     items = InventoryItem.objects.all()
     return render(request, 'business/inventory_list.html', {'items': items})
+def create_expense(request):
+    if request.method == 'POST':
+        form = ExpenseForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('expense_list')
+    else:
+        form = ExpenseForm()
+    return render(request, 'business/create_expense.html', {'form': form})
+
+def expense_list(request):
+    expenses = Expense.objects.all()
+    return render(request, 'business/expense_list.html', {'expenses': expenses})
+
+def profit_and_loss_report(request):
+    total_income = Invoice.objects.aggregate(Sum('total_amount'))['total_amount__sum']
+    total_expenses = Expense.objects.aggregate(Sum('amount'))['amount__sum']
+    profit_or_loss = total_income - total_expenses
+    return render(request, 'business/profit_and_loss_report.html', {
+        'total_income': total_income,
+        'total_expenses': total_expenses,
+        'profit_or_loss': profit_or_loss
+    })
+def create_payment(request):
+    if request.method == 'POST':
+        form = PaymentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('payment_list')
+    else:
+        form = PaymentForm()
+    return render(request, 'business/create_payment.html', {'form': form})
+
+def payment_list(request):
+    payments = Payment.objects.all()
+    return render(request, 'business/payment_list.html', {'payments': payments})
